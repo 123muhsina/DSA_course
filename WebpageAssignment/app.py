ae0d26eba0ea5eb12cda4682f807f5a78b18da06
123muhsina <141301659+123muhsina@users.noreply.github.com>
2023-11-27 16:18:06 +0000

    corrected data

--- WebpageAssignment/app.py
+++ WebpageAssignment/app.py
@@ -7,7 +7,6 @@ from sklearn.metrics import accuracy_score
 app = Flask(__name__)
 
 # Load the dataset
-#dataset_path = 'modeldata/iris.csv'
 df = pd.read_csv("modeldata/iris.csv")
 
 # Drop rows with missing values
@@ -17,7 +16,7 @@ df = df.dropna()
 X = df[['SL', 'SW', 'PL', 'PW']]
 y = df['Classification']
 
-# Train a simple RandomForestClassifier (replace this with your actual model)
+# Train a simple RandomForestClassifier
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
